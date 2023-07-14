6b7af73969c8dd0312880d846c34ef0538b5703a
jmestica <jmestica@inti.gob.ar>
2023-07-14 11:34:12 -0300

    conexión a la base de datos

--- database/database.js
+++ database/database.js
@@ -1,1 +1,11 @@
-//SET PG CONNECTION
+const Pool =  require('pg').Pool
+
+const pool = new Pool({
+    host: process.env.IP_ADDRESS,
+    database: process.env.DB_NAME,
+    password: process.env.PASSWORD,
+    user: process.env.USER,
+    port: process.env.DB_PORT
+})
+
+module.exports = pool
