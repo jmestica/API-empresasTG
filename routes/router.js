3c5bc0ba04da5edebb71d7616240d68ffb19140f
jmestica <jmestica@inti.gob.ar>
2023-07-14 12:45:49 -0300

    controlador y consultas de parques industriales

--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,19 @@
 const express = require("express")
 const router = express.Router()
 
-//Subrutas: empresas, contactos y antecedentes 
+//Subrutas
 const empresasRouter = require("./empresasRouter")
 const contactosRouter = require("./contactosRouter")
 const antecedentesRouter = require("./antecedentesRouter")
 const ciudadesRouter = require( "./ciudadesRouter")
+const parquesRouter = require("./parquesRouter")
+
 
 router.use("/empresas", empresasRouter)
 router.use("/contactos", contactosRouter)
 router.use("/antecedentes", antecedentesRouter)
 router.use("/ciudades", ciudadesRouter)
+router.use('/parques', parquesRouter)
 
 
 module.exports = router
