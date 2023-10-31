1b09eb277a45546bdb4239d0416d720b2bd2240d
Sebastian Antueno <pantueno@inti.gob.ar>
2023-10-31 14:50:40 -0300

    Se finaliza con Buscador de stock

--- app/services/reactivosServices.js
+++ app/services/reactivosServices.js
@@ -47,11 +47,11 @@ const getContador = async () => {
     */
 
 
-    const agregarConsumo = async (nuevoConsumo) => {
-        const values = [nuevoConsumo.cantidad_usada, nuevoConsumo.registro_consumo, nuevoConsumo.cantidad_actual, nuevoConsumo.codigo, nuevoConsumo.nombre_usuario];
-        const res = await db.query('INSERT INTO consumo (cantidad_usada, registro_consumo, cantidad_actual, codigo, nombre_usuario) VALUES ($1, $2, $3, $4, $5)', values);
-        return res.rowCount;
-    }
+const agregarConsumo = async (nuevoConsumo) => {
+    const values = [nuevoConsumo.cantidad_usada, nuevoConsumo.registro_consumo, nuevoConsumo.cantidad_actual, nuevoConsumo.codigo, nuevoConsumo.nombre_usuario];
+    const res = await db.query('INSERT INTO consumo (cantidad_usada, registro_consumo, cantidad_actual, codigo, nombre_usuario) VALUES ($1, $2, $3, $4, $5)', values);
+    return res.rowCount;
+}
 
 
 const getHistorial = async (ID_Reactivo) => {
@@ -96,6 +96,98 @@ const getAll = async () => {
 
 }
 
+const getFiltrados = async (labFilter, tipoFilter, stockFilter) => {
+
+    console.log(labFilter, "labfilter")
+    console.log(tipoFilter, "tipofilter")
+    console.log(stockFilter, "stockFilkter");
+
+    try {
+        let sql = 'SELECT * FROM reactivo WHERE ';
+
+        if (labFilter) {
+            const labInitial = labFilter.charAt(0).toUpperCase(); // Obtén la primera letra del nombre del laboratorio en mayúscula
+            sql += `codigo LIKE $1`;
+            values = [`${labInitial}%`]; // Inicializa el arreglo de valores con la letra del laboratorio
+
+            if (stockFilter === 'En Stock') {
+                sql += ' AND cantidad > 0';
+            } else if (stockFilter === 'Sin Stock') {
+                sql += ' AND (fecha_finalizacion IS NOT NULL)'; // Agrega la condición para cantidad igual o menor a 0 o nula
+            } else if (stockFilter === 'Descartado') {
+                sql += ' AND fecha_descarte IS NOT NULL';
+            } else if (stockFilter === 'Vencido') {
+                sql += ' AND fecha_vto < CURRENT_DATE';
+            }
+
+            if (tipoFilter) {
+                sql += ' AND nombre_reactivo = $2';
+                values.push(tipoFilter);
+            }
+        } else {
+            return null; // Devuelve null si no se especifica el laboratorio
+        }
+
+        // Realiza la consulta a la base de datos con los filtros aplicados
+        const { rows } = await db.query(sql, values);
+
+        return rows.length > 0 ? rows : null;
+    } catch (error) {
+        console.error("Error al obtener los reactivos filtrados", error);
+    }
+
+    // try {
+    //     let sql = 'SELECT * FROM reactivo';
+    //     let values = [null, null]; // Inicializa el arreglo de valores con valores nulos
+
+    //     if (labFilter === 'Cromatografía') {
+    //         sql += ' WHERE codigo LIKE $1 AND nombre_reactivo = $2';
+    //         values[0] = 'C%'; // Primer valor corresponde al marcador $1
+    //         values[1] = tipoFilter; // Segundo valor corresponde al marcador $2
+    //     } else {
+    //         // Otros filtros basados en el laboratorio seleccionado
+    //         if (tipoFilter) {
+    //             sql += ' WHERE nombre_reactivo = $1';
+    //             values[0] = tipoFilter; // Primer valor corresponde al marcador $1
+    //         }
+
+    //         if (stockFilter === 'En Stock') {
+    //             if (values.length > 0) {
+    //                 sql += ' AND cantidad > 0';
+    //             } else {
+    //                 sql += ' WHERE cantidad > 0';
+    //             }
+    //         } else if (stockFilter === 'Fuera de Stock') {
+    //             if (values.length > 0) {
+    //                 sql += ' AND cantidad <= 0';
+    //             } else {
+    //                 sql += ' WHERE cantidad <= 0';
+    //             }
+    //         } else if (stockFilter === 'Descartado') {
+    //             if (values.length > 0) {
+    //                 sql += ' AND fecha_descarte IS NOT NULL';
+    //             } else {
+    //                 sql += ' WHERE fecha_descarte IS NOT NULL';
+    //             }
+    //         } else if (stockFilter === 'Vencido') {
+    //             if (values.length > 0) {
+    //                 sql += ' AND fecha_vto < CURRENT_DATE';
+    //             } else {
+    //                 sql += ' WHERE fecha_vto < CURRENT_DATE';
+    //             }
+    //         }
+    //     }
+
+    //     // Realiza la consulta a la base de datos con los filtros aplicados
+    //     const { rows } = await db.query(sql, values);
+
+    //     return rows.length > 0 ? rows : null;
+
+    // } catch (error) {
+    //     console.error("Error al obtener los reactivos filtrados", error);
+    // }
+}
+
 
 module.exports = {
     getReactivo,
@@ -106,5 +198,6 @@ module.exports = {
     getDatosCompra,
     getAllInfo,
     getAll,
-    getUltimoConsumo
+    getUltimoConsumo,
+    getFiltrados
 }
